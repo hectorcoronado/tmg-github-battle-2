c2995aacff3422d224c41c10921226b432ba1833
hectorcoronado <hectorcoronado.developer@gmail.com>
2019-08-04 08:13:13 -0700

    renders PlayerInput for 2 players

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -4,15 +4,46 @@ import Instructions from './Instructions'
 import PlayerInput from './PlayerInput'
 
 export default class Battle extends React.Component {
+    constructor (props) {
+        super(props)
+
+        this.state = {
+            playerOne: null,
+            playerTwo: null
+        }
+
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleSubmit (id, player) {
+        this.setState({
+            [id]: player
+        })
+    }
+
     render () {
+        const { playerOne, playerTwo } = this.state
         return (
-            <>
+            <React.Fragment>
                 <Instructions />
-                <PlayerInput
-                    label='label!'
-                    onSubmit={(value) => console.log('value:', value) }
-                />
-            </>
+                <div className='players-container'>
+                    <h1 className='center-text header-lg'>Players</h1>
+                    <div className='row space-around'>
+                        {playerOne === null && (
+                            <PlayerInput
+                                label='Player One'
+                                onSubmit={player => this.handleSubmit('playerOne', player)}
+                            />
+                        )}
+                        {playerTwo === null && (
+                            <PlayerInput
+                                label='Player Two'
+                                onSubmit={player => this.handleSubmit('playerTwo', player)}
+                            />
+                        )}
+                    </div>
+                </div>
+            </React.Fragment>
         )
     }
 }
